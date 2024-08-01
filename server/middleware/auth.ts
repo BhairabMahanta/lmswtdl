e4b4b71a577d98bbc957fd71cfc233c601b65179
Akaimnky <bhairabm1908@gmail.com>
2024-08-02 04:23:16 +0530

    fixed some issues with authorization and added db through client

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,60 +1,68 @@
-import {Request, Response, NextFunction} from 'express';
-import jwt, { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
-import { CatchAsyncError } from './catchAsyncErrors';
-import ErrorHandler from '../utils/ErrorHandler';
-import { redis } from '../utils/redis';
-export const isAuthenticated = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
-
-
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload, TokenExpiredError } from "jsonwebtoken";
+import { CatchAsyncError } from "./catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import { redis } from "../utils/redis";
+export const isAuthenticated = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
     const access_token = req.cookies.accessToken;
-    
+
     if (!access_token) {
-
-        return next(new ErrorHandler('Login first to access this resource', 400));
+      return next(new ErrorHandler("Login first to access this resource", 400));
     }
-    try {        
-        if (!process.env.ACCESS_TOKEN) {
-            console.log('ACCESS_TOKEN environment variable is not set');
-            return next(new ErrorHandler('Server configuration error', 500));
-        }
-  // Verify the token
-  const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN as string) as JwtPayload;
+    try {
+      if (!process.env.ACCESS_TOKEN) {
+        console.log("ACCESS_TOKEN environment variable is not set");
+        return next(new ErrorHandler("Server configuration error", 500));
+      }
+      // Verify the token
+      const decoded = jwt.verify(
+        access_token,
+        process.env.ACCESS_TOKEN as string
+      ) as JwtPayload;
 
-  if (!decoded) {
-      return next(new ErrorHandler('Access token is invalid.', 400));
-  }
+      if (!decoded) {
+        return next(new ErrorHandler("Access token is invalid.", 400));
+      }
+      const user = await redis.get(decoded.id);
+      console.log("user:", user);
 
-  const user = await redis.get(decoded.id);
-
-  if (!user) {
-      return next(new ErrorHandler('User not found', 404));
-  }
-  req.user = decoded;
-  console.log("req.user:", req.user);
-  next();
-}catch (error: any) {
-    if (error instanceof TokenExpiredError) {
+      if (!user) {
+        return next(new ErrorHandler("User not found", 404));
+      }
+      req.user = decoded;
+      console.log("req.user:", req.user);
+      next();
+    } catch (error: any) {
+      if (error instanceof TokenExpiredError) {
         console.error("Token expired:", error);
         return res.status(401).json({
-            success: false,
-            message: 'Access token has expired. Please log in again to get a new token.',
+          success: false,
+          message:
+            "Access token has expired. Please log in again to get a new token.",
         });
-    } else {
+      } else {
         console.error("Error during token verification:", error);
         return res.status(401).json({
-            success: false,
-            message: 'Login first to access this resource',
-            error: error.message,
+          success: false,
+          message: "Login first to access this resource",
+          error: error.message,
         });
+      }
     }
-}
-});
+  }
+);
 
 export const authorizeRoles = (...roles: string[]) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        if (!roles.includes(req.user?.role || '')) {
-            return next(new ErrorHandler(`Role (${req.user?.role}) is not allowed to access this resource`, 403));
-        }
-        next();
-    };
-}
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new ErrorHandler(
+          `Role (${req.user?.role}) is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,62 +1,57 @@
-require(    'dotenv').config();
+require("dotenv").config();
 import { Response } from "express";
 import { IUser } from "../models/user.model";
 import { redis } from "./redis";
 interface ITokenOptions {
-    expiresIn: Date;
-    maxAge: number;
-    httpOnly: boolean;
-    sameSite: 'lax' | 'strict' | 'none'| undefined;
-    secure?: boolean;
+  expiresIn: Date;
+  maxAge: number;
+  httpOnly: boolean;
+  sameSite: "lax" | "strict" | "none" | undefined;
+  secure?: boolean;
 }
 
-export const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300', 10);
-export const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '3540', 10);
+const accessTokenExpire = parseInt(
+  process.env.ACCESS_TOKEN_EXPIRE || "300",
+  10
+);
+const refreshTokenExpire = parseInt(
+  process.env.REFRESH_TOKEN_EXPIRE || "3540",
+  10
+);
 
 //cookie optiosn
 export const accessTokenOptions: ITokenOptions = {
-    expiresIn: new Date(Date.now() + accessTokenExpire *60 * 60 * 1000),
-    maxAge: accessTokenExpire * 60* 60*1000,
-    httpOnly: true,
-    sameSite: 'lax'
-}
+  expiresIn: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+  maxAge: accessTokenExpire * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
 export const refreshTokenOptions: ITokenOptions = {
-    expiresIn: new Date(Date.now() + refreshTokenExpire *24*60*60* 1000),
-    maxAge: refreshTokenExpire *24*60*60* 1000,
-    httpOnly: true,
-    sameSite: 'lax'
-}
-export const sendToken =  (user: IUser, statusCode: number, res: Response) => {
-    const accessToken = user.signAccessToken();
-    console.log('accessToken', accessToken);
-    const refreshToken = user.signRefreshToken();
-    console.log('refreshToken', refreshToken);
-    // upload the login session to redis
-    redis.set(user._id, JSON.stringify(user as any));
+  expiresIn: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+  maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
+export const sendToken = (user: IUser, statusCode: number, res: Response) => {
+  const accessToken = user.signAccessToken();
+  const refreshToken = user.signRefreshToken();
+  // upload the login session to redis
+  redis.set(user._id, JSON.stringify(user as any));
 
-    // parsing env  vars
-   
-    if (process.env.NODE_ENV === 'production') {
-        accessTokenOptions.secure = true;
-        refreshTokenOptions.secure = true;
-    }
-        // set the cookies
-        setTimeout(() => {
-           
-       
-    res.cookie('accessToken', accessToken, accessTokenOptions);
-    res.cookie('refreshToken', refreshToken, refreshTokenOptions);
-    console.log('cookies set', res.cookie);
-    console.log("accessToken", accessToken);
-    console.log("refreshToken", refreshToken);
-    console.log("user", user);
-    console.log("accessTokenOptions:", accessTokenOptions)
-    console.log("refreshTokenShit:", refreshTokenOptions)
-        res.status(statusCode).json({
-            success: true,
-            user,
-            accessToken
-        });
-    }, 1900);
-    
-}
+  // parsing env  vars
+
+  if (process.env.NODE_ENV === "production") {
+    accessTokenOptions.secure = true;
+    refreshTokenOptions.secure = true;
+  }
+  // set the cookies
+  setTimeout(() => {
+    res.cookie("accessToken", accessToken, accessTokenOptions);
+    res.cookie("refreshToken", refreshToken, refreshTokenOptions);
+    res.status(statusCode).json({
+      success: true,
+      user,
+      accessToken,
+    });
+  }, 1900);
+};
