f1a00021f2c397056e7ba693f123dab07306014a
Akaimnky <bhairabm1908@gmail.com>
2024-08-02 04:22:49 +0530

    multiple new get/post/put routes *fully tested"

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,11 +1,27 @@
-import express from 'express';
-import { activateAccount ,loginUser,logoutUser,registerUser, updateAccessToken } from '../controllers/user.controller';
-import { authorizeRoles, isAuthenticated } from '../middleware/auth';
+import express from "express";
+import {
+  activateAccount,
+  getUserInfo,
+  loginUser,
+  logoutUser,
+  registerUser,
+  socialAuth,
+  updateAccessToken,
+  updatePassword,
+  updateProfilePicture,
+  updateUserInfo,
+} from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
-userRouter.post('/register', registerUser);
-userRouter.post('/activateAccount', activateAccount);
-userRouter.post('/login', loginUser);
-userRouter.post('/logout',isAuthenticated, logoutUser);
-userRouter.post('/refreshToken', updateAccessToken);
+userRouter.post("/register", registerUser);
+userRouter.post("/activateAccount", activateAccount);
+userRouter.post("/login", loginUser);
+userRouter.post("/logout", isAuthenticated, logoutUser);
+userRouter.get("/refreshToken", updateAccessToken);
+userRouter.get("/userinfo", isAuthenticated, getUserInfo);
+userRouter.post("/socialAuth", socialAuth);
+userRouter.put("/updateUserInfo", isAuthenticated, updateUserInfo);
+userRouter.put("/updatePassword", isAuthenticated, updatePassword);
+userRouter.put("/updateProfile", isAuthenticated, updateProfilePicture);
 export default userRouter;
 //authorizeRoles("admin")
