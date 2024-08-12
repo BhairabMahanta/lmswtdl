8d85729cab48150eff8023f352a590694d5665a6
Akaimnky <bhairabm1908@gmail.com>
2024-08-13 02:07:52 +0530

    started learning frontent (React)

---
+++ client/app/components/header.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react'
+
+interface Props {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+    activeItem: number;
+}
+
+const header:FC<Props> = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default header
---
+++ client/app/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React, {FC, useState} from "react";
+import Heading from "./utils/heading";
+import header from "./components/header";
+interface PageProps {
+  
+}
+const Page: FC<PageProps> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+   return (
+    <div>
+      <Heading title="Eplatform" description="Page description duh" keywords="duh, bro, brainrot" 
+      />
+      <header 
+      open = {open}
+      setOpen = {setOpen}
+      activeItem = {activeItem}
+      />
+    </div>
+    
+  );
+};
+
+export default Page;
